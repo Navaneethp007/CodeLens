df1ad1f8f5c0ac7bbc6327c50aa0d115683a9ac3
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2025-09-28 17:47:32 +0530

    new

--- guide/urls.py
+++ guide/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("upload/", views.upload_and_index_code, name="upload"),
-    path("search/", views.search_code, name="search")
+    path('upload/', views.upload_and_analyze_code, name='upload'),
+    path('search/', views.analyze_code, name='search'),
+    
 ]
