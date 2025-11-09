8411f54292addfd9dbc67b2db5c92973cc9a2a62
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2025-11-09 12:32:25 +0530

    Add new URL patterns for codebase management

--- guide/urls.py
+++ guide/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('upload/', views.upload_file, name='upload_file'),
+    path('upload-codebase/', views.upload_codebase, name='upload_codebase'),
+    path('index-codebase/', views.index_codebase, name='index_codebase'),
     path('query/', views.query_code, name='query_code'),
     path('clear/', views.clear_session, name='clear_session'),
     path('status/', views.file_status, name='file_status'),
